0486e0719654f745b8cd4a90a27bc5fec2ce66ca
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2019-09-25 17:25:13 +0530

    spring core java based config enhancements

--- labs/examples/spring-core-examples/src/main/java/com/examples/spring/core/config/CustomerConfiguration.java
+++ labs/examples/spring-core-examples/src/main/java/com/examples/spring/core/config/CustomerConfiguration.java
@@ -2,13 +2,22 @@ package com.examples.spring.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
+@Import(GreetingsConfig.class)	// import other configuration class
 class CustomerConfiguration 
 {
-    @Bean
+    @Bean(initMethod="init")
     public Customer customer() {
-        return new Customer(person());
+        Customer customer = new Customer(person());	// constructor injection
+        // setter injection
+//        customer.setPerson(person());
+        return customer;
     }
     
     @Bean
--- labs/examples/spring-core-examples/src/main/java/com/examples/spring/core/config/JavaConfigBasedDepedencyInjectionEx02.java
+++ labs/examples/spring-core-examples/src/main/java/com/examples/spring/core/config/JavaConfigBasedDepedencyInjectionEx02.java
@@ -10,6 +10,11 @@ public class JavaConfigBasedDepedencyInjectionEx02 {
 		AbstractApplicationContext context = new AnnotationConfigApplicationContext(CustomerConfiguration.class);
 
 		Customer obj = (Customer) context.getBean("customer");
+		System.out.println("Cutomer: " + obj);
+		System.out.println("Dependent Person: " + obj.getPerson());
+		
+		Person obj1 = (Person) context.getBean("person");
+		System.out.println("Person: " + obj1);
 
 		System.out.println(
 				obj.getPerson().getName() + " " + obj.getPerson().getAge() + " " + obj.getPerson().getLocation());
