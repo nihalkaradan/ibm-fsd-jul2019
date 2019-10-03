627db441ec6fcdd3e27289d7b10a3fb2da5c075d
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2019-10-03 18:20:19 +0530

    employee api spring boot app

---
+++ labs/examples/employee-mgmt-app07/src/main/java/com/examples/empapp/EmployeeMgmtApp07Application.java
@@ -0,0 +1,13 @@
+package com.examples.empapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgmtApp07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgmtApp07Application.class, args);
+	}
+
+}
