51dde53c2d95e40084e5dadd58058c163f6a6544
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2019-10-16 06:25:19 +0530

    lab commands and presentations

--- labs/db/db-scripts.sql
+++ labs/db/db-scripts.sql
@@ -12,6 +12,23 @@ CREATE TABLE jdbctraining.employee (
 	PRIMARY KEY(id)
 );
 
+CREATE SCHEMA hibernate_training;
+
+USE hibernate_training;
+
+CREATE TABLE hibernate_training.employee (
+	id int(11) AUTO_INCREMENT,
+	name varchar(255),
+	age int(3),
+	gender varchar(10),
+	designation varchar(255),
+	department varchar(255),
+	address varchar(255),
+	country varchar(255),
+	contractor boolean,
+	PRIMARY KEY(id)
+);
+
 SELECT * FROM employee;
 
 TRUNCATE employee;
